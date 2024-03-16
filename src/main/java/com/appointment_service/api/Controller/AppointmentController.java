7bfaa82e2b3f1e6118350ebe96efd649d95f3f61
chethangowda1998 <cgowda152@gmail.com>
2024-03-15 22:52:31 -0700

    first commit

--- src/main/java/com/appointment_service/api/Controller/AppointmentController.java
+++ src/main/java/com/appointment_service/api/Controller/AppointmentController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 @RestController
 @RequestMapping("api/appointment")
